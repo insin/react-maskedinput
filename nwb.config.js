adf3467bb9a6aa584bf48f67892f8427f3a950fd
Jonny Buchanan <jonathan.buchanan@gmail.com>
2017-07-04 14:54:34 +1000

    Update nwb: 0.9.x → 0.17.x

--- nwb.config.js
+++ nwb.config.js
@@ -1,11 +1,29 @@
-module.exports = {
-  type: 'react-component',
-  build: {
-    externals: {
-      'react': 'React'
-    },
-    global: 'MaskedInput',
-    jsNext: true,
-    umd: true
+module.exports = function(build) {
+  var config = {
+    type: 'react-component',
+    npm: {
+      umd: {
+        externals: {
+          'react': 'React'
+        },
+        global: 'MaskedInput'
+      }
+    }
   }
+
+  if (/^build/.test(build.command)) {
+    // Don't include default polyfills in the demo build
+    config.polyfill = false
+    // Prevent React 15.x triggering inclusion of the Node.js process shim in the
+    // demo build.
+    config.webpack = {
+      extra: {
+        node: {
+          process: false
+        }
+      }
+    }
+  }
+
+  return config
 }
