89a2687837311a4d50042928c43335ec57f0b7e8
Radu Dunarentu <dunarentu.radu@gmail.com>
2018-06-13 13:41:09 +0300

    (feat): add react strict mode and remove deprecated lifecycles methods

--- demo/src/index.js
+++ demo/src/index.js
@@ -44,69 +44,71 @@ class App extends React.Component {
   }
 
   render() {
-    return <div className="App">
-      <h1>
-        <code>&lt;<a href="https://github.com/insin/react-maskedinput">MaskedInput</a>/&gt;</code>
-      </h1>
-      <p className="lead">A React component which creates a masked <code>&lt;input/&gt;</code></p>
-      <div className="form-field">
-        <label htmlFor="card">Card Number:</label>
-        <MaskedInput mask="1111 1111 1111 1111" name="card" id="card" size="20" value={this.state.card} onChange={this._onChange}/>
+    return (<React.StrictMode>
+      <div className="App">
+        <h1>
+          <code>&lt;<a href="https://github.com/insin/react-maskedinput">MaskedInput</a>/&gt;</code>
+        </h1>
+        <p className="lead">A React component which creates a masked <code>&lt;input/&gt;</code></p>
+        <div className="form-field">
+          <label htmlFor="card">Card Number:</label>
+          <MaskedInput mask="1111 1111 1111 1111" name="card" id="card" size="20" value={this.state.card} onChange={this._onChange}/>
+        </div>
+        <p>You can even externally update the card state like a standard input element:</p>
+        <div className="form-field">
+          <label htmlFor="card">Externally Update:</label>
+          <input onChange={this._onChange} name="card" maxLength="16" style={{borderBottom: '1px solid #999'}} />
+        </div>
+        <p>Placeholders are automatically generated but can be overridden with your own:</p>
+        <div className="form-field">
+          <label htmlFor="expiry">Expiry Date:</label>
+          <MaskedInput mask="11/1111" name="expiry" id="expiry" placeholder="mm/yyyy" onChange={this._onChange}/>
+        </div>
+        <div className="form-field">
+          <label htmlFor="ccv">CCV:</label>
+          <MaskedInput mask="111" name="ccv" id="ccv" onChange={this._onChange}/>
+        </div>
+        <div className="form-field">
+          <label htmlFor="plate">License Plate:</label>
+          <MaskedInput mask="AAA 1111" name="plate" id="plate" onChange={this._onChange} placeholder="ABC 1234"/>
+        </div>
+        <p>Mask placeholder characters can be escaped with a leading <code>\</code> to use them as static contents:</p>
+        <div className="form-field">
+          <label htmlFor="escaped">Escaped:</label>
+          <MaskedInput mask="11 \* 11" name="escaped" id="escaped" onChange={this._onChange}/>
+        </div>
+        <p>Leading static characters:</p>
+        <div className="form-field">
+          <label htmlFor="leading">Leading:</label>
+          <MaskedInput mask="(0) 111 1111" name="leading" id="leading" onChange={this._onChange}/>
+        </div>
+        <p>Changing patterns:</p>
+        <div className="form-field">
+          <label htmlFor="changing">Input:</label>
+          <MaskedInput mask={this.state.pattern} name="changing" id="changing" onChange={this._onChange}/>
+        </div>
+        <div className="form-field">
+          <label htmlFor="pattern">Pattern:</label>
+          <select onChange={this._changePattern}>
+            {PATTERNS.map(pattern => <option value={pattern} key={pattern}>{pattern}</option>)}
+          </select>
+        </div>
+        <p>Dynamically changing the pattern as the user types:</p>
+        <div className="form-field">
+          <label htmlFor="changing">Credit Card:</label>
+          <MaskedInput mask={this.state.cardPattern} name="creditCard" id="creditCard" onChange={this._onCardChange}/>
+        </div>
+        <p>Custom format character (<code>W=[a-zA-Z0-9_]</code>, transformed to uppercase) and placeholder character (en space):</p>
+        <div className="form-field">
+          <label htmlFor="custom">Custom:</label>
+          <CustomInput name="custom" id="custom" onChange={this._onChange}/>
+        </div>
+        <hr/>
+        <pre><code>{JSON.stringify(this.state, null, 2)}</code></pre>
+        <hr/>
+        <footer><a href="https://github.com/insin/react-maskedinput">Source on GitHub</a></footer>
       </div>
-      <p>You can even externally update the card state like a standard input element:</p>
-      <div className="form-field">
-        <label htmlFor="card">Externally Update:</label>
-        <input onChange={this._onChange} name="card" maxLength="16" style={{borderBottom: '1px solid #999'}} />
-      </div>
-      <p>Placeholders are automatically generated but can be overridden with your own:</p>
-      <div className="form-field">
-        <label htmlFor="expiry">Expiry Date:</label>
-        <MaskedInput mask="11/1111" name="expiry" id="expiry" placeholder="mm/yyyy" onChange={this._onChange}/>
-      </div>
-      <div className="form-field">
-        <label htmlFor="ccv">CCV:</label>
-        <MaskedInput mask="111" name="ccv" id="ccv" onChange={this._onChange}/>
-      </div>
-      <div className="form-field">
-        <label htmlFor="plate">License Plate:</label>
-        <MaskedInput mask="AAA 1111" name="plate" id="plate" onChange={this._onChange} placeholder="ABC 1234"/>
-      </div>
-      <p>Mask placeholder characters can be escaped with a leading <code>\</code> to use them as static contents:</p>
-      <div className="form-field">
-        <label htmlFor="escaped">Escaped:</label>
-        <MaskedInput mask="11 \* 11" name="escaped" id="escaped" onChange={this._onChange}/>
-      </div>
-      <p>Leading static characters:</p>
-      <div className="form-field">
-        <label htmlFor="leading">Leading:</label>
-        <MaskedInput mask="(0) 111 1111" name="leading" id="leading" onChange={this._onChange}/>
-      </div>
-      <p>Changing patterns:</p>
-      <div className="form-field">
-        <label htmlFor="changing">Input:</label>
-        <MaskedInput mask={this.state.pattern} name="changing" id="changing" onChange={this._onChange}/>
-      </div>
-      <div className="form-field">
-        <label htmlFor="pattern">Pattern:</label>
-        <select onChange={this._changePattern}>
-          {PATTERNS.map(pattern => <option value={pattern} key={pattern}>{pattern}</option>)}
-        </select>
-      </div>
-      <p>Dynamically changing the pattern as the user types:</p>
-      <div className="form-field">
-        <label htmlFor="changing">Credit Card:</label>
-        <MaskedInput mask={this.state.cardPattern} name="creditCard" id="creditCard" onChange={this._onCardChange}/>
-      </div>
-      <p>Custom format character (<code>W=[a-zA-Z0-9_]</code>, transformed to uppercase) and placeholder character (en space):</p>
-      <div className="form-field">
-        <label htmlFor="custom">Custom:</label>
-        <CustomInput name="custom" id="custom" onChange={this._onChange}/>
-      </div>
-      <hr/>
-      <pre><code>{JSON.stringify(this.state, null, 2)}</code></pre>
-      <hr/>
-      <footer><a href="https://github.com/insin/react-maskedinput">Source on GitHub</a></footer>
-    </div>
+    </React.StrictMode>)
   }
 }
 
